441ec721414035d42f9c8893e9f6c1fd8bd640e1
Steve Gerbino <steve@gerbino.co>
2021-02-25 14:45:42 -0500

    Work in progress

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,15 @@
+hunter_config(Boost
+   VERSION ${HUNTER_Boost_VERSION}
+   CMAKE_ARGS
+      USE_CONFIG_FROM_BOOST=ON
+      Boost_USE_STATIC_LIBS=ON
+      Boost_NO_BOOST_CMAKE=ON
+)
+
+hunter_config(koinos_log
+   URL "https://github.com/koinos/koinos-log-cpp/archive/v0.1.0.tar.gz"
+   SHA1 "bc27602cd82a3f3849796ec0d6de37d3841a19ea"
+   CMAKE_ARGS
+      BUILD_TESTS=OFF
+)
+ 
