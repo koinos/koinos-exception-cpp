bc31dec1602cbf74bc50f128d0014d16addb7197
Steve Gerbino <steve@gerbino.co>
2021-02-28 15:36:46 -0500

    Use hunterized Koinos Types and add _GNU_SOURCE flag

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -12,4 +12,11 @@ hunter_config(koinos_log
    CMAKE_ARGS
       BUILD_TESTS=OFF
 )
+
+hunter_config(koinos_types
+   URL "https://github.com/koinos/koinos-types/archive/v0.1.0-alpha.tar.gz"
+   SHA1 "0bd263981ae87ef1276ff4f9a483d48ba1277277"
+   CMAKE_ARGS
+      BUILD_TESTS=OFF
+)
  
