373937ced4b890bc6a8dbdad6452560860a38f5e
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-03-03 17:53:17 -0800

    Update hunter config

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -7,16 +7,15 @@ hunter_config(Boost
 )
 
 hunter_config(koinos_log
-   URL "https://github.com/koinos/koinos-log-cpp/archive/4ecb8399d05d1639c52a34845f55aa826f35d484.tar.gz"
+   URL  "https://github.com/koinos/koinos-log-cpp/archive/4ecb8399d05d1639c52a34845f55aa826f35d484.tar.gz"
    SHA1 "1b11e2acadd4d37a483944096bed916ba579637d"
    CMAKE_ARGS
       BUILD_TESTS=OFF
 )
 
 hunter_config(koinos_types
-   URL "https://github.com/koinos/koinos-types/archive/d8a9db91761d8aa84723f0b0b5b12e032fad9fa9.tar.gz"
-   SHA1 "3764a668d7e0f6c5876f522f11bdc39cbdbbba8b"
+   URL "https://github.com/koinos/koinos-types-cpp/archive/89164de74999fbc95562ccf524f01d95aa401937.tar.gz"
+   SHA1 "846eb8340edd72f4c280690a15fb3af1ee0b9bd7"
    CMAKE_ARGS
       BUILD_TESTS=OFF
 )
- 
